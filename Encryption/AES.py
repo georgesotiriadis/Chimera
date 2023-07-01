958d89f3cd4071b807e41e1f542faf80aa896c73
efchatz <43434138+efchatz@users.noreply.github.com>
2023-07-01 18:58:51 +0300

    Update AES.py

--- Encryption/AES.py
+++ Encryption/AES.py
@@ -4,8 +4,11 @@ from Crypto.Cipher import AES
 from secrets import token_bytes
 from binascii import unhexlify
 import hashlib
+import random
 
-keyAES = token_bytes(16)
+#keyAES = token_bytes(16)
+key = ''.join([chr(random.randint(0, 255)) for i in range(16)])
+keyAES = ''.join(['\\x' + hex(ord(x))[2:].zfill(2) for x in key])
 
 def encryptAES(plaintext,keyAES):
     
