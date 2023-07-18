f1ac4db0bacaf78fc0b4aa1050bc797905610ce7
efchatz <43434138+efchatz@users.noreply.github.com>
2023-07-19 02:43:10 +0300

    Update Polymorphic1.py

--- Evasion/Polymorphic1.py
+++ Evasion/Polymorphic1.py
@@ -4,10 +4,9 @@ import string
 def generate_junk_function_cpp():
     num_iterations = random.randint(1000, 5000)
 
-    code = f'''#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
+    value = random.choice(range(1,100))
+    
+    code = f'''
 
 class ComplexNumber {{
 private:
@@ -35,7 +34,7 @@ public:
     }}
 
     double getMagnitude() const {{
-        return std::sqrt(real * real + imaginary * imaginary);
+        return sqrt(real * real + imaginary * imaginary);
     }}
 
     ComplexNumber power(int exponent) const {{
@@ -48,40 +47,42 @@ public:
         return result;
     }}
 
-    std::string toString() const {{
-        std::string output;
+    char* toString() {{
+    const int bufferSize = 256;
+    char* output = new char[bufferSize];
+    output[0] = '\0';
 
-        if (real != 0.0 && imaginary != 0.0) {{
-            output += std::to_string(real) + " + " + std::to_string(imaginary) + "i";
-        }}
-        else if (real != 0.0) {{
-            output += std::to_string(real);
-        }}
-        else if (imaginary != 0.0) {{
-            output += std::to_string(imaginary) + "i";
-        }}
-        else {{
-            output += "0";
-        }}
-
-        return output;
+    if (real != 0.0 && imaginary != 0.0) {{
+        snprintf(output, bufferSize, "%.2f + %.2fi", real, imaginary);
     }}
+    else if (real != 0.0) {{
+        snprintf(output, bufferSize, "%.2f", real);
+    }}
+    else if (imaginary != 0.0) {{
+        snprintf(output, bufferSize, "%.2fi", imaginary);
+    }}
+    else {{
+        snprintf(output, bufferSize, "0");
+    }}
+
+    return output;
+    }};
 }};
 
-void junkFunction() {{
-    std::srand(std::time(0));
+void junkFunction{str(value)}() {{
+    srand(time(0));
 
-    int numIterations = {num_iterations};
+    int numIterations = 10;
 
     char* junkMemory = new char[numIterations];
     for (int i = 0; i < numIterations; ++i) {{
-        junkMemory[i] = std::rand() % 256;
+        junkMemory[i] = rand() % 256;
     }}
 
     ComplexNumber* complexNumbers = new ComplexNumber[numIterations];
     for (int i = 0; i < numIterations; ++i) {{
-        double real = std::rand() % 101 - 50;
-        double imaginary = std::rand() % 101 - 50;
+        double real = rand() % 101 - 50;
+        double imaginary = rand() % 101 - 50;
         complexNumbers[i] = ComplexNumber(real, imaginary);
     }}
 
@@ -101,37 +102,15 @@ void junkFunction() {{
         }}
     }}
 
-    ComplexNumber powerResult = complexNumbers[maxMagnitudeIndex].power(std::rand() % 6 + 2);
+    ComplexNumber powerResult = complexNumbers[maxMagnitudeIndex].power(rand() % 6 + 2);
 
-    std::string output;
+    // Do whatever you want with the results here
 
-    output += "Sum of complex numbers: ";
-    output += sum.toString();
-    output += "\\n";
-
-    output += "Product of complex numbers: ";
-    output += product.toString();
-    output += "\\n";
-
-    output += "Complex number with the maximum magnitude: ";
-    output += complexNumbers[maxMagnitudeIndex].toString();
-    output += "\\n";
-
-    output += "Result of powering the complex number: ";
-    output += powerResult.toString();
-    output += "\\n";
-
-    delete[] junkMemory;
-    delete[] complexNumbers;
-
-    // You can do whatever you want with the 'output' string here
 }}
 
-int main() {{
-    junkFunction();
 
-    return 0;
-}}
+void junkFunction{str(value)}();
+
 '''
 
     return code
@@ -139,4 +118,4 @@ int main() {{
 
 def generate_cpp_script():
     cpp_code = generate_junk_function_cpp()
-    return f'''{cpp_code}'''
+    return cpp_code
