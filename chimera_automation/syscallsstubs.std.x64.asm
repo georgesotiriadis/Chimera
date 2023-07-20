1461312868e64d818d199d42258d37d876ee9848
georgesotiriadis <sotiriadisgeorge2@gmail.com>
2023-07-20 16:59:46 +0300

    sys3
    updated to sys3 for test

--- chimera_automation/syscallsstubs.std.x64.asm
+++ chimera_automation/syscallsstubs.std.x64.asm
@@ -1,24 +1,21 @@
-.data
-currentHash DWORD 0
-
 .code
-EXTERN SW2_GetSyscallNumber: PROC
-    
-WhisperMain PROC
-    pop rax
-    mov [rsp+ 8], rcx              ; Save registers.
-    mov [rsp+16], rdx
-    mov [rsp+24], r8
-    mov [rsp+32], r9
-    sub rsp, 28h
-    mov ecx, currentHash
-    call SW2_GetSyscallNumber
-    add rsp, 28h
-    mov rcx, [rsp+ 8]              ; Restore registers.
-    mov rdx, [rsp+16]
-    mov r8, [rsp+24]
-    mov r9, [rsp+32]
-    mov r15,rcx
+
+EXTERN SW3_GetSyscallNumber: PROC
+
+NtProtectVirtualMemory PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 00F99051Bh        ; Load function hash into ECX.
+	call SW3_GetSyscallNumber              ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp+8]                      ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r15,rcx
     mov r14,r15
     mov r13,r14
     mov r10,r13
@@ -32,33 +29,132 @@ WhisperMain PROC
     nop
     nop
     nop
-    syscall                        ; Issue syscall
-    ret
-WhisperMain ENDP
-
-NtResumeThread PROC
-    mov currentHash, 03A9E2023h    ; Load function hash into global variable.
-    call WhisperMain               ; Resolve function hash into syscall number and make the call
-NtResumeThread ENDP
-
-NtQueueApcThread PROC
-    mov currentHash, 07651EC6Fh    ; Load function hash into global variable.
-    call WhisperMain               ; Resolve function hash into syscall number and make the call
-NtQueueApcThread ENDP
-
-NtAllocateVirtualMemory PROC
-    mov currentHash, 0039128C3h    ; Load function hash into global variable.
-    call WhisperMain               ; Resolve function hash into syscall number and make the call
-NtAllocateVirtualMemory ENDP
-
-NtProtectVirtualMemory PROC
-    mov currentHash, 007971F07h    ; Load function hash into global variable.
-    call WhisperMain               ; Resolve function hash into syscall number and make the call
+	syscall                    ; Invoke system call.
+	ret
 NtProtectVirtualMemory ENDP
 
 NtWriteVirtualMemory PROC
-    mov currentHash, 0CD9930E2h    ; Load function hash into global variable.
-    call WhisperMain               ; Resolve function hash into syscall number and make the call
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 0DB942197h        ; Load function hash into ECX.
+	call SW3_GetSyscallNumber              ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp+8]                      ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r15,rcx
+    mov r14,r15
+    mov r13,r14
+    mov r10,r13
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+	syscall                    ; Invoke system call.
+	ret
 NtWriteVirtualMemory ENDP
+
+NtAllocateVirtualMemory PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 00595031Bh        ; Load function hash into ECX.
+	call SW3_GetSyscallNumber              ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp+8]                      ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r15,rcx
+    mov r14,r15
+    mov r13,r14
+    mov r10,r13
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+	syscall                    ; Invoke system call.
+	ret
+NtAllocateVirtualMemory ENDP
+
+NtQueueApcThread PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 09A20419Fh        ; Load function hash into ECX.
+	call SW3_GetSyscallNumber              ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp+8]                      ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r15,rcx
+    mov r14,r15
+    mov r13,r14
+    mov r10,r13
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+	syscall                    ; Invoke system call.
+	ret
+NtQueueApcThread ENDP
+
+NtResumeThread PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	mov ecx, 074CE3A67h        ; Load function hash into ECX.
+	call SW3_GetSyscallNumber              ; Resolve function hash into syscall number.
+	add rsp, 28h
+	mov rcx, [rsp+8]                      ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r15,rcx
+    mov r14,r15
+    mov r13,r14
+    mov r10,r13
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+	syscall                    ; Invoke system call.
+	ret
+NtResumeThread ENDP
 
 end
